76061b7b81a122dc8b83caf3959c7f9834bf498a
swati sharma <swati.sharma2615@gmail.com>
2022-08-29 14:05:42 +0530

    Added Euereka

---
+++ src/test/java/com/example/euraeka/EuraekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euraeka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuraekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
